0481c5dfb6a6314eea6b63430be27eef0d141859
Michel Hardy <michel.robin.hardy@gmail.com>
2021-02-18 21:16:49 +0100

    first iteration

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 // Iteration 1: Names and Input
-
+console.log("I'm ready")
+var hacker1= "Michel"
+var hacker2= "Karina"
+console.log("The driver's name is "+ hacker1)
+console.log("The navigator's name is "+ hacker2)
 
 // Iteration 2: Conditionals
 
